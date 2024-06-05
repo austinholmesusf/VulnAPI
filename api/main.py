aaf95323c558b7124a5d1f33d0cfee538c107774
austinhco <austinholmes@usf.edu>
2024-06-04 20:41:11 -0400

    Added authentication routing

--- api/main.py
+++ api/main.py
@@ -3,12 +3,13 @@ from flask_cors import CORS
 
 import api.repository.DBConnector as DB
 from api.controller.ControllerAdvice import configure_controller_advice
+from api.controller.AuthController import configure_auth_routes
 
 
 # Configure endpoints
 app = Flask(__name__)
 CORS(app)
-
+configure_auth_routes(app)
 
 # Controller Error Handler
 configure_controller_advice(app)
